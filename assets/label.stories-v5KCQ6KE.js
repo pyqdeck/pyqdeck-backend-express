637f5dcd9d8357b38f71d1cf06dcc2b0e42ff86d
hasanraiyan <hasanraiyan@users.noreply.github.com>
2026-05-02 12:24:54 +0000

    deploy: bbd71ef1cd76fadb7893438e78be3ef19746c280

--- assets/label.stories-v5KCQ6KE.js
+++
@@ -1,8 +0,0 @@
-import{n as e,s as t}from"./chunk-CzyJ72yW.js";import{Br as n}from"./iframe-v22JUpF8.js";import{n as r,t as i}from"./label-RldEARXf.js";import{n as a,t as o}from"./input-TbaXZ8i7.js";var s,c,l,u;e((()=>{s=t(n(),1),r(),a(),c={title:`UI/Label`,component:i,tags:[`autodocs`]},l={render:e=>(0,s.jsxs)(`div`,{className:`grid w-full max-w-sm items-center gap-1.5`,children:[(0,s.jsx)(i,{htmlFor:`email`,...e,children:`Email`}),(0,s.jsx)(o,{type:`email`,id:`email`,placeholder:`Email`})]})},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
-  render: args => <div className="grid w-full max-w-sm items-center gap-1.5">
-      <Label htmlFor="email" {...args}>
-        Email
-      </Label>
-      <Input type="email" id="email" placeholder="Email" />
-    </div>
-}`,...l.parameters?.docs?.source}}},u=[`Default`]}))();export{l as Default,u as __namedExportsOrder,c as default};
