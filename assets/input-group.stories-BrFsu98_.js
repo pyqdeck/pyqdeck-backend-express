688842667de2ed853dfd36f2d0f6dc3fc2968298
hasanraiyan <hasanraiyan@users.noreply.github.com>
2026-05-02 11:51:59 +0000

    deploy: 32f9ccce9d8a7fc17e495db9a77ed2ffec0167f8

---
+++ assets/input-group.stories-BrFsu98_.js
@@ -0,0 +1,30 @@
+import{n as e,s as t}from"./chunk-CzyJ72yW.js";import{Br as n}from"./iframe-v22JUpF8.js";import{T as r,_ as i,g as a,t as o}from"./lucide-react-CxBRhVC1.js";import{a as s,i as c,n as l,o as u,r as d,t as f}from"./input-group-CmEW3_Xp.js";var p,m,h,g,_,v;e((()=>{p=t(n(),1),u(),o(),m={title:`UI/InputGroup`,component:f,tags:[`autodocs`]},h={render:()=>(0,p.jsxs)(f,{className:`max-w-sm`,children:[(0,p.jsx)(l,{children:(0,p.jsx)(r,{className:`size-4`})}),(0,p.jsx)(c,{placeholder:`Email address`})]})},g={render:()=>(0,p.jsxs)(f,{className:`max-w-sm`,children:[(0,p.jsx)(c,{placeholder:`Search...`}),(0,p.jsx)(l,{align:`inline-end`,children:(0,p.jsxs)(d,{children:[(0,p.jsx)(i,{className:`size-4`}),`Search`]})})]})},_={render:()=>(0,p.jsxs)(f,{className:`max-w-sm`,children:[(0,p.jsx)(l,{children:(0,p.jsx)(s,{children:`https://`})}),(0,p.jsx)(c,{placeholder:`example.com`}),(0,p.jsx)(l,{align:`inline-end`,children:(0,p.jsx)(d,{variant:`primary`,children:(0,p.jsx)(a,{className:`size-4`})})})]})},h.parameters={...h.parameters,docs:{...h.parameters?.docs,source:{originalSource:`{
+  render: () => <InputGroup className="max-w-sm">
+      <InputGroupAddon>
+        <Mail className="size-4" />
+      </InputGroupAddon>
+      <InputGroupInput placeholder="Email address" />
+    </InputGroup>
+}`,...h.parameters?.docs?.source}}},g.parameters={...g.parameters,docs:{...g.parameters?.docs,source:{originalSource:`{
+  render: () => <InputGroup className="max-w-sm">
+      <InputGroupInput placeholder="Search..." />
+      <InputGroupAddon align="inline-end">
+        <InputGroupButton>
+          <Search className="size-4" />
+          Search
+        </InputGroupButton>
+      </InputGroupAddon>
+    </InputGroup>
+}`,...g.parameters?.docs?.source}}},_.parameters={..._.parameters,docs:{..._.parameters?.docs,source:{originalSource:`{
+  render: () => <InputGroup className="max-w-sm">
+      <InputGroupAddon>
+        <InputGroupText>https://</InputGroupText>
+      </InputGroupAddon>
+      <InputGroupInput placeholder="example.com" />
+      <InputGroupAddon align="inline-end">
+        <InputGroupButton variant="primary">
+          <Send className="size-4" />
+        </InputGroupButton>
+      </InputGroupAddon>
+    </InputGroup>
+}`,..._.parameters?.docs?.source}}},v=[`WithIcon`,`WithButton`,`WithPrefixAndSuffix`]}))();export{g as WithButton,h as WithIcon,_ as WithPrefixAndSuffix,v as __namedExportsOrder,m as default};
---
+++ assets/menubar.stories-BsD786qr.js
@@ -0,0 +1,75 @@
+import{n as e,s as t}from"./chunk-CzyJ72yW.js";import{Br as n,Ct as r,St as i,_t as a,bt as o,dt as s,ft as c,gt as l,ht as u,lt as d,mt as f,o as p,pt as m,s as h,u as g,ut as _,vt as v,wt as y,xi as b,xt as x,yt as S}from"./iframe-v22JUpF8.js";import{K as C,Y as w,t as T}from"./lucide-react-CxBRhVC1.js";function E({className:e,...t}){return(0,H.jsx)(S,{"data-slot":`menubar`,className:p(`flex h-8 items-center gap-0.5 rounded-lg border p-[3px]`,e),...t})}function D({...e}){return(0,H.jsx)(u,{"data-slot":`menubar-menu`,...e})}function O({...e}){return(0,H.jsx)(s,{"data-slot":`menubar-group`,...e})}function k({...e}){return(0,H.jsx)(l,{"data-slot":`menubar-portal`,...e})}function A({...e}){return(0,H.jsx)(a,{"data-slot":`menubar-radio-group`,...e})}function j({className:e,...t}){return(0,H.jsx)(y,{"data-slot":`menubar-trigger`,className:p(`hover:bg-muted aria-expanded:bg-muted flex items-center rounded-sm px-1.5 py-[2px] text-sm font-medium outline-hidden select-none`,e),...t})}function M({className:e,align:t=`start`,alignOffset:n=-4,sideOffset:r=8,...i}){return(0,H.jsx)(k,{children:(0,H.jsx)(_,{"data-slot":`menubar-content`,align:t,alignOffset:n,sideOffset:r,className:p(`bg-popover text-popover-foreground ring-foreground/10 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 data-open:animate-in data-open:fade-in-0 data-open:zoom-in-95 z-50 min-w-36 origin-(--radix-menubar-content-transform-origin) overflow-hidden rounded-lg p-1 shadow-md ring-1 duration-100`,e),...i})})}function N({className:e,inset:t,variant:n=`default`,...r}){return(0,H.jsx)(c,{"data-slot":`menubar-item`,"data-inset":t,"data-variant":n,className:p(`group/menubar-item focus:bg-accent focus:text-accent-foreground not-data-[variant=destructive]:focus:**:text-accent-foreground data-[variant=destructive]:text-destructive data-[variant=destructive]:focus:bg-destructive/10 data-[variant=destructive]:focus:text-destructive dark:data-[variant=destructive]:focus:bg-destructive/20 data-[variant=destructive]:*:[svg]:text-destructive! relative flex cursor-default items-center gap-1.5 rounded-md px-1.5 py-1 text-sm outline-hidden select-none data-disabled:pointer-events-none data-disabled:opacity-50 data-inset:pl-7 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4`,e),...r})}function P({className:e,children:t,checked:n,inset:r,...i}){return(0,H.jsxs)(d,{"data-slot":`menubar-checkbox-item`,"data-inset":r,className:p(`focus:bg-accent focus:text-accent-foreground focus:**:text-accent-foreground relative flex cursor-default items-center gap-1.5 rounded-md py-1 pr-1.5 pl-7 text-sm outline-hidden select-none data-disabled:pointer-events-none data-inset:pl-7 [&_svg]:pointer-events-none [&_svg]:shrink-0`,e),checked:n,...i,children:[(0,H.jsx)(`span`,{className:`pointer-events-none absolute left-1.5 flex size-4 items-center justify-center [&_svg:not([class*='size-'])]:size-4`,children:(0,H.jsx)(m,{children:(0,H.jsx)(w,{})})}),t]})}function F({className:e,children:t,inset:n,...r}){return(0,H.jsxs)(v,{"data-slot":`menubar-radio-item`,"data-inset":n,className:p(`focus:bg-accent focus:text-accent-foreground focus:**:text-accent-foreground relative flex cursor-default items-center gap-1.5 rounded-md py-1 pr-1.5 pl-7 text-sm outline-hidden select-none data-disabled:pointer-events-none data-disabled:opacity-50 data-inset:pl-7 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4`,e),...r,children:[(0,H.jsx)(`span`,{className:`pointer-events-none absolute left-1.5 flex size-4 items-center justify-center [&_svg:not([class*='size-'])]:size-4`,children:(0,H.jsx)(m,{children:(0,H.jsx)(w,{})})}),t]})}function I({className:e,inset:t,...n}){return(0,H.jsx)(f,{"data-slot":`menubar-label`,"data-inset":t,className:p(`px-1.5 py-1 text-sm font-medium data-inset:pl-7`,e),...n})}function L({className:e,...t}){return(0,H.jsx)(o,{"data-slot":`menubar-separator`,className:p(`bg-border -mx-1 my-1 h-px`,e),...t})}function R({className:e,...t}){return(0,H.jsx)(`span`,{"data-slot":`menubar-shortcut`,className:p(`text-muted-foreground group-focus/menubar-item:text-accent-foreground ml-auto text-xs tracking-widest`,e),...t})}function z({...e}){return(0,H.jsx)(x,{"data-slot":`menubar-sub`,...e})}function B({className:e,inset:t,children:n,...i}){return(0,H.jsxs)(r,{"data-slot":`menubar-sub-trigger`,"data-inset":t,className:p(`focus:bg-accent focus:text-accent-foreground data-open:bg-accent data-open:text-accent-foreground flex cursor-default items-center gap-1.5 rounded-md px-1.5 py-1 text-sm outline-none select-none data-inset:pl-7 [&_svg:not([class*='size-'])]:size-4`,e),...i,children:[n,(0,H.jsx)(C,{className:`ml-auto size-4`})]})}function V({className:e,...t}){return(0,H.jsx)(i,{"data-slot":`menubar-sub-content`,className:p(`bg-popover text-popover-foreground ring-foreground/10 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 data-open:animate-in data-open:fade-in-0 data-open:zoom-in-95 data-closed:animate-out data-closed:fade-out-0 data-closed:zoom-out-95 z-50 min-w-32 origin-(--radix-menubar-content-transform-origin) overflow-hidden rounded-lg p-1 shadow-lg ring-1 duration-100`,e),...t})}var H,U=e((()=>{H=t(n(),1),b(),g(),h(),T(),E.__docgenInfo={description:``,methods:[],displayName:`Menubar`},k.__docgenInfo={description:``,methods:[],displayName:`MenubarPortal`},D.__docgenInfo={description:``,methods:[],displayName:`MenubarMenu`},j.__docgenInfo={description:``,methods:[],displayName:`MenubarTrigger`},M.__docgenInfo={description:``,methods:[],displayName:`MenubarContent`,props:{align:{defaultValue:{value:`'start'`,computed:!1},required:!1},alignOffset:{defaultValue:{value:`-4`,computed:!1},required:!1},sideOffset:{defaultValue:{value:`8`,computed:!1},required:!1}}},O.__docgenInfo={description:``,methods:[],displayName:`MenubarGroup`},L.__docgenInfo={description:``,methods:[],displayName:`MenubarSeparator`},I.__docgenInfo={description:``,methods:[],displayName:`MenubarLabel`},N.__docgenInfo={description:``,methods:[],displayName:`MenubarItem`,props:{variant:{defaultValue:{value:`'default'`,computed:!1},required:!1}}},R.__docgenInfo={description:``,methods:[],displayName:`MenubarShortcut`},P.__docgenInfo={description:``,methods:[],displayName:`MenubarCheckboxItem`},A.__docgenInfo={description:``,methods:[],displayName:`MenubarRadioGroup`},F.__docgenInfo={description:``,methods:[],displayName:`MenubarRadioItem`},z.__docgenInfo={description:``,methods:[],displayName:`MenubarSub`},B.__docgenInfo={description:``,methods:[],displayName:`MenubarSubTrigger`},V.__docgenInfo={description:``,methods:[],displayName:`MenubarSubContent`}})),W,G,K,q;e((()=>{W=t(n(),1),U(),G={title:`UI/Menubar`,component:E,tags:[`autodocs`]},K={render:()=>(0,W.jsxs)(E,{children:[(0,W.jsxs)(D,{children:[(0,W.jsx)(j,{children:`File`}),(0,W.jsxs)(M,{children:[(0,W.jsxs)(N,{children:[`New Tab `,(0,W.jsx)(R,{children:`⌘T`})]}),(0,W.jsxs)(N,{children:[`New Window `,(0,W.jsx)(R,{children:`⌘N`})]}),(0,W.jsx)(N,{disabled:!0,children:`New Incognito Window`}),(0,W.jsx)(L,{}),(0,W.jsxs)(z,{children:[(0,W.jsx)(B,{children:`Share`}),(0,W.jsxs)(V,{children:[(0,W.jsx)(N,{children:`Email link`}),(0,W.jsx)(N,{children:`Messages`}),(0,W.jsx)(N,{children:`Notes`})]})]}),(0,W.jsx)(L,{}),(0,W.jsxs)(N,{children:[`Print... `,(0,W.jsx)(R,{children:`⌘P`})]})]})]}),(0,W.jsxs)(D,{children:[(0,W.jsx)(j,{children:`Edit`}),(0,W.jsxs)(M,{children:[(0,W.jsxs)(N,{children:[`Undo `,(0,W.jsx)(R,{children:`⌘Z`})]}),(0,W.jsxs)(N,{children:[`Redo `,(0,W.jsx)(R,{children:`⇧⌘Z`})]}),(0,W.jsx)(L,{}),(0,W.jsxs)(z,{children:[(0,W.jsx)(B,{children:`Find`}),(0,W.jsxs)(V,{children:[(0,W.jsx)(N,{children:`Search the web`}),(0,W.jsx)(L,{}),(0,W.jsx)(N,{children:`Find...`}),(0,W.jsx)(N,{children:`Find Next`}),(0,W.jsx)(N,{children:`Find Previous`})]})]}),(0,W.jsx)(L,{}),(0,W.jsx)(N,{children:`Cut`}),(0,W.jsx)(N,{children:`Copy`}),(0,W.jsx)(N,{children:`Paste`})]})]}),(0,W.jsxs)(D,{children:[(0,W.jsx)(j,{children:`View`}),(0,W.jsxs)(M,{children:[(0,W.jsx)(P,{children:`Always Show Bookmarks Bar`}),(0,W.jsx)(P,{checked:!0,children:`Always Show Full URLs`}),(0,W.jsx)(L,{}),(0,W.jsxs)(N,{inset:!0,children:[`Reload `,(0,W.jsx)(R,{children:`⌘R`})]}),(0,W.jsxs)(N,{disabled:!0,inset:!0,children:[`Force Reload `,(0,W.jsx)(R,{children:`⇧⌘R`})]}),(0,W.jsx)(L,{}),(0,W.jsx)(N,{inset:!0,children:`Toggle Fullscreen`}),(0,W.jsx)(L,{}),(0,W.jsx)(N,{inset:!0,children:`Hide Sidebar`})]})]})]})},K.parameters={...K.parameters,docs:{...K.parameters?.docs,source:{originalSource:`{
+  render: () => <Menubar>
+      <MenubarMenu>
+        <MenubarTrigger>File</MenubarTrigger>
+        <MenubarContent>
+          <MenubarItem>
+            New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+          </MenubarItem>
+          <MenubarItem>
+            New Window <MenubarShortcut>⌘N</MenubarShortcut>
+          </MenubarItem>
+          <MenubarItem disabled>New Incognito Window</MenubarItem>
+          <MenubarSeparator />
+          <MenubarSub>
+            <MenubarSubTrigger>Share</MenubarSubTrigger>
+            <MenubarSubContent>
+              <MenubarItem>Email link</MenubarItem>
+              <MenubarItem>Messages</MenubarItem>
+              <MenubarItem>Notes</MenubarItem>
+            </MenubarSubContent>
+          </MenubarSub>
+          <MenubarSeparator />
+          <MenubarItem>
+            Print... <MenubarShortcut>⌘P</MenubarShortcut>
+          </MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+      <MenubarMenu>
+        <MenubarTrigger>Edit</MenubarTrigger>
+        <MenubarContent>
+          <MenubarItem>
+            Undo <MenubarShortcut>⌘Z</MenubarShortcut>
+          </MenubarItem>
+          <MenubarItem>
+            Redo <MenubarShortcut>⇧⌘Z</MenubarShortcut>
+          </MenubarItem>
+          <MenubarSeparator />
+          <MenubarSub>
+            <MenubarSubTrigger>Find</MenubarSubTrigger>
+            <MenubarSubContent>
+              <MenubarItem>Search the web</MenubarItem>
+              <MenubarSeparator />
+              <MenubarItem>Find...</MenubarItem>
+              <MenubarItem>Find Next</MenubarItem>
+              <MenubarItem>Find Previous</MenubarItem>
+            </MenubarSubContent>
+          </MenubarSub>
+          <MenubarSeparator />
+          <MenubarItem>Cut</MenubarItem>
+          <MenubarItem>Copy</MenubarItem>
+          <MenubarItem>Paste</MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+      <MenubarMenu>
+        <MenubarTrigger>View</MenubarTrigger>
+        <MenubarContent>
+          <MenubarCheckboxItem>Always Show Bookmarks Bar</MenubarCheckboxItem>
+          <MenubarCheckboxItem checked>
+            Always Show Full URLs
+          </MenubarCheckboxItem>
+          <MenubarSeparator />
+          <MenubarItem inset>
+            Reload <MenubarShortcut>⌘R</MenubarShortcut>
+          </MenubarItem>
+          <MenubarItem disabled inset>
+            Force Reload <MenubarShortcut>⇧⌘R</MenubarShortcut>
+          </MenubarItem>
+          <MenubarSeparator />
+          <MenubarItem inset>Toggle Fullscreen</MenubarItem>
+          <MenubarSeparator />
+          <MenubarItem inset>Hide Sidebar</MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+    </Menubar>
+}`,...K.parameters?.docs?.source}}},q=[`Default`]}))();export{K as Default,q as __namedExportsOrder,G as default};
