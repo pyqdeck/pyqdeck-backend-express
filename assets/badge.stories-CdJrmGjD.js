422b129faa6a2942b86cfe19f91c9fe4064358a4
hasanraiyan <hasanraiyan@users.noreply.github.com>
2026-05-02 18:33:04 +0000

    deploy: 245647c2f033161afc5976813a9989a3a23835fb

---
+++ assets/badge.stories-CdJrmGjD.js
@@ -0,0 +1,41 @@
+import{n as e}from"./chunk-CzyJ72yW.js";import{n as t,t as n}from"./badge-2bbDw6BW.js";var r,i,a,o,s,c,l,u,d,f;e((()=>{t(),r={title:`UI/Badge`,component:n,tags:[`autodocs`],argTypes:{variant:{control:{type:`select`},options:[`default`,`secondary`,`destructive`,`outline`,`ghost`,`link`,`teal`,`purple`,`amber`,`emerald`]}}},i={args:{children:`Badge`,variant:`default`}},a={args:{children:`Secondary`,variant:`secondary`}},o={args:{children:`Destructive`,variant:`destructive`}},s={args:{children:`Outline`,variant:`outline`}},c={args:{children:`New Paper`,variant:`teal`}},l={args:{children:`Module 1`,variant:`purple`}},u={args:{children:`High Weightage`,variant:`amber`}},d={args:{children:`Solved`,variant:`emerald`}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Badge',
+    variant: 'default'
+  }
+}`,...i.parameters?.docs?.source}}},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Secondary',
+    variant: 'secondary'
+  }
+}`,...a.parameters?.docs?.source}}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Destructive',
+    variant: 'destructive'
+  }
+}`,...o.parameters?.docs?.source}}},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Outline',
+    variant: 'outline'
+  }
+}`,...s.parameters?.docs?.source}}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'New Paper',
+    variant: 'teal'
+  }
+}`,...c.parameters?.docs?.source}}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Module 1',
+    variant: 'purple'
+  }
+}`,...l.parameters?.docs?.source}}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'High Weightage',
+    variant: 'amber'
+  }
+}`,...u.parameters?.docs?.source}}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Solved',
+    variant: 'emerald'
+  }
+}`,...d.parameters?.docs?.source}}},f=[`Default`,`Secondary`,`Destructive`,`Outline`,`Teal`,`Purple`,`Amber`,`Emerald`]}))();export{u as Amber,i as Default,o as Destructive,d as Emerald,s as Outline,l as Purple,a as Secondary,c as Teal,f as __namedExportsOrder,r as default};
