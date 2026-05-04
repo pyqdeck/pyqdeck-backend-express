35e2a667a08030c868df11ca5b3221d02351c2b1
hasanraiyan <hasanraiyan@users.noreply.github.com>
2026-05-04 02:58:41 +0000

    deploy: e8a598fd05bfc11a9cdb2462b37261b8c1044852

---
+++ assets/skeleton.stories-ClKfT_gn.js
@@ -0,0 +1,17 @@
+import{n as e,s as t}from"./chunk-CzyJ72yW.js";import{Br as n}from"./iframe-DksX-Qjr.js";import{n as r,t as i}from"./skeleton-CE1duJr_.js";var a,o,s,c,l;e((()=>{a=t(n(),1),r(),o={title:`UI/Skeleton`,component:i,tags:[`autodocs`]},s={render:()=>(0,a.jsxs)(`div`,{className:`flex flex-col space-y-3`,children:[(0,a.jsx)(i,{className:`h-[125px] w-[250px] rounded-xl`}),(0,a.jsxs)(`div`,{className:`space-y-2`,children:[(0,a.jsx)(i,{className:`h-4 w-[250px]`}),(0,a.jsx)(i,{className:`h-4 w-[200px]`})]})]})},c={render:()=>(0,a.jsxs)(`div`,{className:`flex items-center space-x-4`,children:[(0,a.jsx)(i,{className:`h-12 w-12 rounded-full`}),(0,a.jsxs)(`div`,{className:`space-y-2`,children:[(0,a.jsx)(i,{className:`h-4 w-[250px]`}),(0,a.jsx)(i,{className:`h-4 w-[200px]`})]})]})},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  render: () => <div className="flex flex-col space-y-3">
+      <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[200px]" />
+      </div>
+    </div>
+}`,...s.parameters?.docs?.source}}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: () => <div className="flex items-center space-x-4">
+      <Skeleton className="h-12 w-12 rounded-full" />
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[200px]" />
+      </div>
+    </div>
+}`,...c.parameters?.docs?.source}}},l=[`Default`,`Profile`]}))();export{s as Default,c as Profile,l as __namedExportsOrder,o as default};
