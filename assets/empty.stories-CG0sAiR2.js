525a30713be8abad696d7762cff2c029e2b418ba
hasanraiyan <hasanraiyan@users.noreply.github.com>
2026-05-04 00:28:47 +0000

    deploy: 060101e3bfabf821682c9b90da8b5062ec074497

---
+++ assets/empty.stories-CG0sAiR2.js
@@ -0,0 +1,16 @@
+import{n as e,s as t}from"./chunk-CzyJ72yW.js";import{Br as n}from"./iframe-BOTGc3BM.js";import{k as r,t as i}from"./lucide-react-Drft9pDt.js";import{r as a,t as o}from"./button-Dxm2T9T9.js";import{a as s,i as c,n as l,o as u,r as d,s as f,t as p}from"./empty-CL4YYXxu.js";var m,h,g,_;e((()=>{m=t(n(),1),f(),i(),a(),h={title:`UI/Empty`,component:p,tags:[`autodocs`]},g={render:()=>(0,m.jsxs)(p,{children:[(0,m.jsxs)(c,{children:[(0,m.jsx)(s,{variant:`icon`,children:(0,m.jsx)(r,{})}),(0,m.jsx)(u,{children:`No projects found`}),(0,m.jsx)(d,{children:`Get started by creating a new project.`})]}),(0,m.jsx)(l,{children:(0,m.jsx)(o,{children:`Create project`})})]})},g.parameters={...g.parameters,docs:{...g.parameters?.docs,source:{originalSource:`{
+  render: () => <Empty>
+      <EmptyHeader>
+        <EmptyMedia variant="icon">
+          <PackageOpen />
+        </EmptyMedia>
+        <EmptyTitle>No projects found</EmptyTitle>
+        <EmptyDescription>
+          Get started by creating a new project.
+        </EmptyDescription>
+      </EmptyHeader>
+      <EmptyContent>
+        <Button>Create project</Button>
+      </EmptyContent>
+    </Empty>
+}`,...g.parameters?.docs?.source}}},_=[`Default`]}))();export{g as Default,_ as __namedExportsOrder,h as default};
