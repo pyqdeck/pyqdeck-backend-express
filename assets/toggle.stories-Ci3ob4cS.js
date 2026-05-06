7332b056feadac10e5c2067b7d97c1d48579b416
hasanraiyan <hasanraiyan@users.noreply.github.com>
2026-05-06 01:38:56 +0000

    deploy: 8c88327fe407eaa346d90ff136d8efc38c301307

---
+++ assets/toggle.stories-Ci3ob4cS.js
@@ -0,0 +1,12 @@
+import{n as e,s as t}from"./chunk-CzyJ72yW.js";import{Br as n}from"./iframe-C595Sw-a.js";import{V as r,kt as i,t as a}from"./lucide-react-QYEQinjD.js";import{n as o,t as s}from"./toggle-DKILgLWV.js";var c,l,u,d,f;e((()=>{c=t(n(),1),o(),a(),l={title:`UI/Toggle`,component:s,tags:[`autodocs`],argTypes:{variant:{control:{type:`select`},options:[`default`,`outline`]},size:{control:{type:`select`},options:[`default`,`sm`,`lg`]}}},u={render:e=>(0,c.jsx)(s,{...e,"aria-label":`Toggle bold`,children:(0,c.jsx)(i,{className:`h-4 w-4`})})},d={args:{variant:`outline`},render:e=>(0,c.jsx)(s,{...e,"aria-label":`Toggle italic`,children:(0,c.jsx)(r,{className:`h-4 w-4`})})},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  render: args => <Toggle {...args} aria-label="Toggle bold">
+      <Bold className="h-4 w-4" />
+    </Toggle>
+}`,...u.parameters?.docs?.source}}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  args: {
+    variant: 'outline'
+  },
+  render: args => <Toggle {...args} aria-label="Toggle italic">
+      <Italic className="h-4 w-4" />
+    </Toggle>
+}`,...d.parameters?.docs?.source}}},f=[`Default`,`Outline`]}))();export{u as Default,d as Outline,f as __namedExportsOrder,l as default};
