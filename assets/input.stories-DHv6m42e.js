6b6e490d9336a1ce0c3f4ef7402728b6d310644e
hasanraiyan <hasanraiyan@users.noreply.github.com>
2026-05-06 16:15:32 +0000

    deploy: c4a3e4cf56b6bda8c9ecc0b6e8866b70f44cfaa3

---
+++ assets/input.stories-DHv6m42e.js
@@ -0,0 +1,15 @@
+import{n as e}from"./chunk-CzyJ72yW.js";import{n as t,t as n}from"./input-CeeCU5Ex.js";var r,i,a,o,s;e((()=>{t(),r={title:`UI/Input`,component:n,tags:[`autodocs`],argTypes:{type:{control:{type:`select`},options:[`text`,`password`,`email`,`number`,`file`]}}},i={args:{placeholder:`Email`,type:`email`}},a={args:{disabled:!0,placeholder:`Disabled input`}},o={args:{type:`file`}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  args: {
+    placeholder: 'Email',
+    type: 'email'
+  }
+}`,...i.parameters?.docs?.source}}},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    disabled: true,
+    placeholder: 'Disabled input'
+  }
+}`,...a.parameters?.docs?.source}}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    type: 'file'
+  }
+}`,...o.parameters?.docs?.source}}},s=[`Default`,`Disabled`,`File`]}))();export{i as Default,a as Disabled,o as File,s as __namedExportsOrder,r as default};
