6cfa6ef2d78d2fe62031c6df8183aba386f189cf
hasanraiyan <hasanraiyan@users.noreply.github.com>
2026-05-06 08:33:36 +0000

    deploy: 987f51fad7fe16ebb693fe2955f5c5a669f90f0f

---
+++ assets/pagination.stories-C6y762Kz.js
@@ -0,0 +1,26 @@
+import{n as e,s as t}from"./chunk-CzyJ72yW.js";import{Br as n}from"./iframe-3apzxnj8.js";import{a as r,c as i,i as a,n as o,o as s,r as c,s as l,t as u}from"./pagination-B_AGis4k.js";var d,f,p,m;e((()=>{d=t(n(),1),i(),f={title:`UI/Pagination`,component:u,tags:[`autodocs`]},p={render:()=>(0,d.jsx)(u,{children:(0,d.jsxs)(o,{children:[(0,d.jsx)(a,{children:(0,d.jsx)(l,{href:`#`})}),(0,d.jsx)(a,{children:(0,d.jsx)(r,{href:`#`,children:`1`})}),(0,d.jsx)(a,{children:(0,d.jsx)(r,{href:`#`,isActive:!0,children:`2`})}),(0,d.jsx)(a,{children:(0,d.jsx)(r,{href:`#`,children:`3`})}),(0,d.jsx)(a,{children:(0,d.jsx)(c,{})}),(0,d.jsx)(a,{children:(0,d.jsx)(s,{href:`#`})})]})})},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  render: () => <Pagination>
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious href="#" />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">1</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#" isActive>
+            2
+          </PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">3</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationEllipsis />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationNext href="#" />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+}`,...p.parameters?.docs?.source}}},m=[`Default`]}))();export{p as Default,m as __namedExportsOrder,f as default};
