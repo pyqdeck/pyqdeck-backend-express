b3161ec102aec3eb414509547e9b0886c56cc1f3
hasanraiyan <hasanraiyan@users.noreply.github.com>
2026-05-06 16:26:29 +0000

    deploy: 3290b941c2c8694b2c22e758b4a9bceb06c337c2

---
+++ assets/spinner.stories-DTx225OD.js
@@ -0,0 +1,11 @@
+import{n as e,s as t}from"./chunk-CzyJ72yW.js";import{Br as n,o as r,s as i}from"./iframe-DUTTb83f.js";import{B as a,t as o}from"./lucide-react-BLlIbcqz.js";function s({className:e,...t}){return(0,c.jsx)(a,{role:`status`,"aria-label":`Loading`,className:r(`size-4 animate-spin`,e),...t})}var c,l=e((()=>{c=t(n(),1),i(),o(),s.__docgenInfo={description:``,methods:[],displayName:`Spinner`}})),u,d,f,p,m,h;e((()=>{u=t(n(),1),l(),d={title:`UI/Spinner`,component:s,tags:[`autodocs`]},f={render:e=>(0,u.jsx)(s,{...e})},p={args:{className:`size-8`}},m={args:{className:`text-primary`}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  render: args => <Spinner {...args} />
+}`,...f.parameters?.docs?.source}}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  args: {
+    className: 'size-8'
+  }
+}`,...p.parameters?.docs?.source}}},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  args: {
+    className: 'text-primary'
+  }
+}`,...m.parameters?.docs?.source}}},h=[`Default`,`Large`,`CustomColor`]}))();export{m as CustomColor,f as Default,p as Large,h as __namedExportsOrder,d as default};
