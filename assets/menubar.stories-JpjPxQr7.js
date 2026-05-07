f3617ded6d4b1c5f3e743d134bc3362798ae8739
hasanraiyan <hasanraiyan@users.noreply.github.com>
2026-05-07 02:24:17 +0000

    deploy: c8325266fc1bb58e1897104cd4269be4300b22bb

---
+++ assets/menubar.stories-JpjPxQr7.js
@@ -0,0 +1,92 @@
+import{n as e,s as t}from"./chunk-CzyJ72yW.js";import{Br as n,Ci as r,Ct as i,St as a,_t as o,bt as s,dt as c,ft as l,gt as u,ht as d,lt as f,mt as p,o as m,pt as h,s as g,u as _,ut as v,vt as y,wt as b,xt as x,yt as S}from"./iframe-DU5cTs9q.js";import{n as C,r as w}from"./dist-BvuEP38C.js";import{Ct as T,Et as E,t as D}from"./lucide-react-vHEy438o.js";function O({className:e,...t}){return(0,W.jsx)(S,{"data-slot":`menubar`,className:m(`flex h-8 items-center gap-0.5 rounded-lg border p-[3px]`,e),...t})}function k({...e}){return(0,W.jsx)(d,{"data-slot":`menubar-menu`,...e})}function A({...e}){return(0,W.jsx)(c,{"data-slot":`menubar-group`,...e})}function j({...e}){return(0,W.jsx)(u,{"data-slot":`menubar-portal`,...e})}function M({...e}){return(0,W.jsx)(o,{"data-slot":`menubar-radio-group`,...e})}function N({className:e,...t}){return(0,W.jsx)(b,{"data-slot":`menubar-trigger`,className:m(`hover:bg-muted aria-expanded:bg-muted flex items-center rounded-sm px-1.5 py-[2px] text-sm font-medium outline-hidden select-none`,e),...t})}function P({className:e,align:t=`start`,alignOffset:n=-4,sideOffset:r=8,...i}){return(0,W.jsx)(j,{children:(0,W.jsx)(v,{"data-slot":`menubar-content`,align:t,alignOffset:n,sideOffset:r,className:m(`bg-popover text-popover-foreground ring-foreground/10 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 data-open:animate-in data-open:fade-in-0 data-open:zoom-in-95 z-50 min-w-36 origin-(--radix-menubar-content-transform-origin) overflow-hidden rounded-lg p-1 shadow-md ring-1 duration-100`,e),...i})})}function F({className:e,inset:t,variant:n=`default`,...r}){return(0,W.jsx)(l,{"data-slot":`menubar-item`,"data-inset":t,"data-variant":n,className:m(`group/menubar-item focus:bg-accent focus:text-accent-foreground not-data-[variant=destructive]:focus:**:text-accent-foreground data-[variant=destructive]:text-destructive data-[variant=destructive]:focus:bg-destructive/10 data-[variant=destructive]:focus:text-destructive dark:data-[variant=destructive]:focus:bg-destructive/20 data-[variant=destructive]:*:[svg]:text-destructive! relative flex cursor-default items-center gap-1.5 rounded-md px-1.5 py-1 text-sm outline-hidden select-none data-disabled:pointer-events-none data-disabled:opacity-50 data-inset:pl-7 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4`,e),...r})}function I({className:e,children:t,checked:n,inset:r,...i}){return(0,W.jsxs)(f,{"data-slot":`menubar-checkbox-item`,"data-inset":r,className:m(`focus:bg-accent focus:text-accent-foreground focus:**:text-accent-foreground relative flex cursor-default items-center gap-1.5 rounded-md py-1 pr-1.5 pl-7 text-sm outline-hidden select-none data-disabled:pointer-events-none data-inset:pl-7 [&_svg]:pointer-events-none [&_svg]:shrink-0`,e),checked:n,...i,children:[(0,W.jsx)(`span`,{className:`pointer-events-none absolute left-1.5 flex size-4 items-center justify-center [&_svg:not([class*='size-'])]:size-4`,children:(0,W.jsx)(h,{children:(0,W.jsx)(E,{})})}),t]})}function L({className:e,children:t,inset:n,...r}){return(0,W.jsxs)(y,{"data-slot":`menubar-radio-item`,"data-inset":n,className:m(`focus:bg-accent focus:text-accent-foreground focus:**:text-accent-foreground relative flex cursor-default items-center gap-1.5 rounded-md py-1 pr-1.5 pl-7 text-sm outline-hidden select-none data-disabled:pointer-events-none data-disabled:opacity-50 data-inset:pl-7 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4`,e),...r,children:[(0,W.jsx)(`span`,{className:`pointer-events-none absolute left-1.5 flex size-4 items-center justify-center [&_svg:not([class*='size-'])]:size-4`,children:(0,W.jsx)(h,{children:(0,W.jsx)(E,{})})}),t]})}function R({className:e,inset:t,...n}){return(0,W.jsx)(p,{"data-slot":`menubar-label`,"data-inset":t,className:m(`px-1.5 py-1 text-sm font-medium data-inset:pl-7`,e),...n})}function z({className:e,...t}){return(0,W.jsx)(s,{"data-slot":`menubar-separator`,className:m(`bg-border -mx-1 my-1 h-px`,e),...t})}function B({className:e,...t}){return(0,W.jsx)(`span`,{"data-slot":`menubar-shortcut`,className:m(`text-muted-foreground group-focus/menubar-item:text-accent-foreground ml-auto text-xs tracking-widest`,e),...t})}function V({...e}){return(0,W.jsx)(x,{"data-slot":`menubar-sub`,...e})}function H({className:e,inset:t,children:n,...r}){return(0,W.jsxs)(i,{"data-slot":`menubar-sub-trigger`,"data-inset":t,className:m(`focus:bg-accent focus:text-accent-foreground data-open:bg-accent data-open:text-accent-foreground flex cursor-default items-center gap-1.5 rounded-md px-1.5 py-1 text-sm outline-none select-none data-inset:pl-7 [&_svg:not([class*='size-'])]:size-4`,e),...r,children:[n,(0,W.jsx)(T,{className:`ml-auto size-4`})]})}function U({className:e,...t}){return(0,W.jsx)(a,{"data-slot":`menubar-sub-content`,className:m(`bg-popover text-popover-foreground ring-foreground/10 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 data-open:animate-in data-open:fade-in-0 data-open:zoom-in-95 data-closed:animate-out data-closed:fade-out-0 data-closed:zoom-out-95 z-50 min-w-32 origin-(--radix-menubar-content-transform-origin) overflow-hidden rounded-lg p-1 ring-1 duration-100`,e),...t})}var W,G=e((()=>{W=t(n(),1),r(),_(),g(),D(),O.__docgenInfo={description:``,methods:[],displayName:`Menubar`},j.__docgenInfo={description:``,methods:[],displayName:`MenubarPortal`},k.__docgenInfo={description:``,methods:[],displayName:`MenubarMenu`},N.__docgenInfo={description:``,methods:[],displayName:`MenubarTrigger`},P.__docgenInfo={description:``,methods:[],displayName:`MenubarContent`,props:{align:{defaultValue:{value:`'start'`,computed:!1},required:!1},alignOffset:{defaultValue:{value:`-4`,computed:!1},required:!1},sideOffset:{defaultValue:{value:`8`,computed:!1},required:!1}}},A.__docgenInfo={description:``,methods:[],displayName:`MenubarGroup`},z.__docgenInfo={description:``,methods:[],displayName:`MenubarSeparator`},R.__docgenInfo={description:``,methods:[],displayName:`MenubarLabel`},F.__docgenInfo={description:``,methods:[],displayName:`MenubarItem`,props:{variant:{defaultValue:{value:`'default'`,computed:!1},required:!1}}},B.__docgenInfo={description:``,methods:[],displayName:`MenubarShortcut`},I.__docgenInfo={description:``,methods:[],displayName:`MenubarCheckboxItem`},M.__docgenInfo={description:``,methods:[],displayName:`MenubarRadioGroup`},L.__docgenInfo={description:``,methods:[],displayName:`MenubarRadioItem`},V.__docgenInfo={description:``,methods:[],displayName:`MenubarSub`},H.__docgenInfo={description:``,methods:[],displayName:`MenubarSubTrigger`},U.__docgenInfo={description:``,methods:[],displayName:`MenubarSubContent`}})),K,q,J,Y,X;e((()=>{K=t(n(),1),w(),G(),q={title:`UI/Menubar`,component:O,tags:[`autodocs`],argTypes:{className:{control:`text`,description:`Additional CSS classes for the menubar.`}},parameters:{docs:{description:{component:`A horizontal menu bar typically visible at the top of an application, contains buttons that open menus.`}}}},J={render:e=>(0,K.jsxs)(O,{...e,children:[(0,K.jsxs)(k,{children:[(0,K.jsx)(N,{children:`File`}),(0,K.jsxs)(P,{children:[(0,K.jsxs)(F,{onSelect:C(),children:[`New Tab `,(0,K.jsx)(B,{children:`⌘T`})]}),(0,K.jsxs)(F,{onSelect:C(),children:[`New Window `,(0,K.jsx)(B,{children:`⌘N`})]}),(0,K.jsx)(F,{disabled:!0,onSelect:C(),children:`New Incognito Window`}),(0,K.jsx)(z,{}),(0,K.jsx)(F,{onSelect:C(),children:`Share`}),(0,K.jsx)(z,{}),(0,K.jsx)(F,{onSelect:C(),children:`Print`})]})]}),(0,K.jsxs)(k,{children:[(0,K.jsx)(N,{children:`Edit`}),(0,K.jsxs)(P,{children:[(0,K.jsx)(F,{onSelect:C(),children:`Undo`}),(0,K.jsx)(F,{onSelect:C(),children:`Redo`})]})]})]}),args:{}},Y={render:e=>(0,K.jsxs)(O,{...e,children:[(0,K.jsxs)(k,{children:[(0,K.jsx)(N,{children:`Academics`}),(0,K.jsxs)(P,{children:[(0,K.jsxs)(F,{onSelect:C(),children:[`New Branch `,(0,K.jsx)(B,{children:`⌘N`})]}),(0,K.jsxs)(F,{onSelect:C(),children:[`Import CSV `,(0,K.jsx)(B,{children:`⌘I`})]}),(0,K.jsx)(z,{}),(0,K.jsxs)(V,{children:[(0,K.jsx)(H,{children:`Syllabus`}),(0,K.jsxs)(U,{children:[(0,K.jsx)(F,{onSelect:C(),children:`View All`}),(0,K.jsx)(F,{onSelect:C(),children:`Export PDF`})]})]}),(0,K.jsx)(z,{}),(0,K.jsx)(F,{variant:`destructive`,onSelect:C(),children:`Delete All Data`})]})]}),(0,K.jsxs)(k,{children:[(0,K.jsx)(N,{children:`View`}),(0,K.jsxs)(P,{children:[(0,K.jsx)(I,{onSelect:C(),children:`Show Sidebar`}),(0,K.jsx)(I,{checked:!0,onSelect:C(),children:`Show Analytics`}),(0,K.jsx)(z,{}),(0,K.jsxs)(F,{inset:!0,onSelect:C(),children:[`Reload `,(0,K.jsx)(B,{children:`⌘R`})]})]})]}),(0,K.jsxs)(k,{children:[(0,K.jsx)(N,{children:`Profiles`}),(0,K.jsx)(P,{children:(0,K.jsxs)(M,{value:`admin`,children:[(0,K.jsx)(R,{inset:!0,children:`Role`}),(0,K.jsx)(z,{}),(0,K.jsx)(L,{value:`admin`,onSelect:C(),children:`Admin`}),(0,K.jsx)(L,{value:`moderator`,onSelect:C(),children:`Moderator`}),(0,K.jsx)(L,{value:`student`,onSelect:C(),children:`Student`})]})})]})]}),args:{className:`w-fit`}},J.parameters={...J.parameters,docs:{...J.parameters?.docs,source:{originalSource:`{
+  render: args => <Menubar {...args}>
+      <MenubarMenu>
+        <MenubarTrigger>File</MenubarTrigger>
+        <MenubarContent>
+          <MenubarItem onSelect={fn()}>
+            New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+          </MenubarItem>
+          <MenubarItem onSelect={fn()}>
+            New Window <MenubarShortcut>⌘N</MenubarShortcut>
+          </MenubarItem>
+          <MenubarItem disabled onSelect={fn()}>
+            New Incognito Window
+          </MenubarItem>
+          <MenubarSeparator />
+          <MenubarItem onSelect={fn()}>Share</MenubarItem>
+          <MenubarSeparator />
+          <MenubarItem onSelect={fn()}>Print</MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+      <MenubarMenu>
+        <MenubarTrigger>Edit</MenubarTrigger>
+        <MenubarContent>
+          <MenubarItem onSelect={fn()}>Undo</MenubarItem>
+          <MenubarItem onSelect={fn()}>Redo</MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+    </Menubar>,
+  args: {}
+}`,...J.parameters?.docs?.source}}},Y.parameters={...Y.parameters,docs:{...Y.parameters?.docs,source:{originalSource:`{
+  render: args => <Menubar {...args}>
+      <MenubarMenu>
+        <MenubarTrigger>Academics</MenubarTrigger>
+        <MenubarContent>
+          <MenubarItem onSelect={fn()}>
+            New Branch <MenubarShortcut>⌘N</MenubarShortcut>
+          </MenubarItem>
+          <MenubarItem onSelect={fn()}>
+            Import CSV <MenubarShortcut>⌘I</MenubarShortcut>
+          </MenubarItem>
+          <MenubarSeparator />
+          <MenubarSub>
+            <MenubarSubTrigger>Syllabus</MenubarSubTrigger>
+            <MenubarSubContent>
+              <MenubarItem onSelect={fn()}>View All</MenubarItem>
+              <MenubarItem onSelect={fn()}>Export PDF</MenubarItem>
+            </MenubarSubContent>
+          </MenubarSub>
+          <MenubarSeparator />
+          <MenubarItem variant="destructive" onSelect={fn()}>
+            Delete All Data
+          </MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+      <MenubarMenu>
+        <MenubarTrigger>View</MenubarTrigger>
+        <MenubarContent>
+          <MenubarCheckboxItem onSelect={fn()}>
+            Show Sidebar
+          </MenubarCheckboxItem>
+          <MenubarCheckboxItem checked onSelect={fn()}>
+            Show Analytics
+          </MenubarCheckboxItem>
+          <MenubarSeparator />
+          <MenubarItem inset onSelect={fn()}>
+            Reload <MenubarShortcut>⌘R</MenubarShortcut>
+          </MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+      <MenubarMenu>
+        <MenubarTrigger>Profiles</MenubarTrigger>
+        <MenubarContent>
+          <MenubarRadioGroup value="admin">
+            <MenubarLabel inset>Role</MenubarLabel>
+            <MenubarSeparator />
+            <MenubarRadioItem value="admin" onSelect={fn()}>
+              Admin
+            </MenubarRadioItem>
+            <MenubarRadioItem value="moderator" onSelect={fn()}>
+              Moderator
+            </MenubarRadioItem>
+            <MenubarRadioItem value="student" onSelect={fn()}>
+              Student
+            </MenubarRadioItem>
+          </MenubarRadioGroup>
+        </MenubarContent>
+      </MenubarMenu>
+    </Menubar>,
+  args: {
+    className: 'w-fit'
+  }
+}`,...Y.parameters?.docs?.source}}},X=[`Default`,`Complex`]}))();export{Y as Complex,J as Default,X as __namedExportsOrder,q as default};
